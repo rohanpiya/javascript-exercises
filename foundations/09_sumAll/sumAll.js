51066969bfe0212b86bcbe6c50ce37a75ef89703
rohanpiya <piyar@dickinson.edu>
2026-06-16 14:30:16 +0545

    Complete sumAll Exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let total = 0;
+    if (
+        (num1 < 0 || num2 < 0) ||
+        (typeof num1 != "number" || typeof num2 != "number") ||
+        (Number.isInteger(num1) == false || Number.isInteger(num2) == false)
+    ) {
+        return "ERROR";
+    }
+    if (num1 > num2) {
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+    for(let i = num1; i <= num2; i++){
+        total += i
+    }
+    return total
 };
 
 // Do not edit below this line
