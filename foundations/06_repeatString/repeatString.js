e491f7da947f4c91ec5f428c42370a34df251ecb
rohanpiya <piyar@dickinson.edu>
2026-06-16 14:04:33 +0545

    Clean up code

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,13 +1,10 @@
 const repeatString = function(str, num) {
-    if (num < 0) {
-        return "ERROR";
-    } else {
-        let newStr = '';
-        for(let i = 0; i < num; i++){
-            newStr = newStr+str;
-        }
-        return newStr;
+    if (num < 0) return "ERROR";
+    let newStr = '';
+    for(let i = 0; i < num; i++){
+        newStr = newStr + str;
     }
+    return newStr;
 };
 
 // Do not edit below this line
