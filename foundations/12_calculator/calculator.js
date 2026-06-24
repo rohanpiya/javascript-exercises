8e9040b5ab0dc97c7471b8f2bf4f7ab1797ce95e
rohanpiya <piyar@dickinson.edu>
2026-06-24 13:01:45 +0545

    Complete 12 calculator js foundations ex

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(numArray) {
+	return numArray.reduce((total, num) => {
+    return total + num;
+  }, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(numArray) {
+  return numArray.reduce((total, num) => {
+    return total * num;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  fac = 1;
+	for (let i = 2; i <= num; i++){
+    fac = fac * i;
+  }
+  return fac;
 };
 
 // Do not edit below this line
