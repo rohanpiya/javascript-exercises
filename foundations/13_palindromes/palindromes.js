21673cae0f558741f91e5a6d9b8e651fc77d41d7
rohanpiya <piyar@dickinson.edu>
2026-06-24 13:20:03 +0545

    Complete palindromes js ex 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = removePunctuations(str).toLowerCase();
+    str = removeSpaces(str);
+    const reverseStr = reverseString(str).toLowerCase();
+    if(str == reverseStr){
+        return true;
+    }
+    return false;
 };
+
+function removePunctuations(str) {
+  return str.replace(/[^\w\s]/g, "").toLowerCase();;
+}
+
+function removeSpaces(str) {
+  return str.replace(/\s/g, "");
+}
+
+const reverseString = function(str) {
+    return str.split("").reverse().join("");
+}
 
 // Do not edit below this line
 module.exports = palindromes;
