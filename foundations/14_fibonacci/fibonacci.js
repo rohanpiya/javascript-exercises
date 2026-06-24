6a50db0a9cd773883c9229c23b3b0a82f55e5cd7
rohanpiya <piyar@dickinson.edu>
2026-06-24 13:31:13 +0545

    Complete js 14 ex: fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+
+    if(num == 0) {
+        return 0;
+    } if (num == 1) {
+        return 1;
+    } else {
+        return fibonacci(num - 1) + fibonacci(num - 2);
+    }
 
 };
 
