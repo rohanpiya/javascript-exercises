ec5f611ccfad0b9c972aeaa10445135bded5cadb
rohanpiya <piyar@dickinson.edu>
2026-06-24 13:47:41 +0545

    Complete js foundations ex

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const currentYear = new Date().getFullYear();
+    arr.sort((a,b) => {
+        if(a.yearOfDeath == undefined){
+            a.yearOfDeath == currentYear;
+        } else if (b.yearOfDeath == undefined) {
+            b.yearOfDeath = currentYear;
+        }
+        
+        if((a.yearOfDeath - a.yearOfBirth) < (b.yearOfDeath - b.yearOfBirth)){
+            return 1;
+        } else {
+            return -1;
+        }
+    })
+    return arr[0];
 };
 
 // Do not edit below this line
